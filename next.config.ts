f58e28815a47b0a614e9e2463dbdde268b454b46
ThongLai <laiminhthong1@gmail.com>
2025-05-07 15:32:39 +0100

    chore(configure files): Update project configurations and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
